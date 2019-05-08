cb7df31c222a32149c76ad46ef1cc1cdd04a8346
Mckay <mckaywrigley@gmail.com>
2019-05-08 10:25:11 -0700

    configures knexfile

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
-
-const port = 4000;
 
 const authRoutes = require("./routes/auth");
 
 app.use("/api/auth", authRoutes);
+
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}.`);
