df7636295b5395b36b1162572c6ef78107a26440
Mckay <mckaywrigley@gmail.com>
2019-05-08 10:13:46 -0700

    generateToken function

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
 
 const router = express.Router();
 
@@ -7,5 +9,18 @@ router.post("/register", (req, res) => {});
 router.post("/login", (req, res) => {});
 
 router.get("/users", (req, res) => {});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1h"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
