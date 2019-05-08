929cc7776fd3594600415e9722418212c5324e3a
Mckay <mckaywrigley@gmail.com>
2019-05-08 10:03:32 -0700

    sets up authRoutes

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 4000;
 
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth", authRoutes);
+
 app.listen(port, () => {
   console.log(`Server running on port: ${port}.`);
 });
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {});
+
+router.post("/login", (req, res) => {});
+
+router.get("/users", (req, res) => {});
+
+module.exports = router;
