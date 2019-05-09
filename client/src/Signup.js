af527a36265447c2a76d9219ea58b9be3d926b60
Mckay <mckaywrigley@gmail.com>
2019-05-09 09:06:21 -0700

    creates signup component

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,10 +1,64 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      department: ""
+    };
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:5000/api/register`, this.state)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    this.setState({
+      username: "",
+      password: "",
+      department: ""
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Signup</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            placeholder="username"
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            placeholder="password"
+          />
+          <input
+            type="text"
+            name="department"
+            value={this.state.department}
+            onChange={this.handleChange}
+            placeholder="department"
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
       </div>
     );
   }
