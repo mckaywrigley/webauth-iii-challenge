ba29ed74551b74526291e205fbeef3b6b0447f41
Mckay <mckaywrigley@gmail.com>
2019-05-09 09:20:54 -0700

    creates users component

---
+++ client/src/Users.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/users`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          users: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    if (this.state.users.length === 0) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <h2>Users</h2>
+        {this.state.users.map(user => {
+          return (
+            <div key={user.id}>
+              <p>{user.username}</p>
+              <p>{user.department}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Users;
